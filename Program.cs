e39df0d282d532c6d4f170d953451f1c0f4589be
Janabai Tukaram Tidke <janatidke2407@gmail.com>
2021-09-01 00:14:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PalindromeCharArray
+{
+    class CharPalindrome
+    {
+        internal bool CheckPalindrome(char[] name)
+        {
+            int i, j, n;
+            bool palindrome = true;
+            n = name.Length;
+            for (i = 0, j = n - 1; i < n / 2; i++, j--)
+            {
+                if (name[i] != name[j])
+                {
+                    palindrome = false;
+                    break;
+                }
+            }
+            return palindrome;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] name = new char[10];
+            Console.Write("Enter any string: ");
+            name = Console.ReadLine().ToCharArray();
+            Console.WriteLine("Palindrome: " + new CharPalindrome().CheckPalindrome(name));
+        }
+    }
+}
